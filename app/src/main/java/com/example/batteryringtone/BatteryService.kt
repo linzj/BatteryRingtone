0a83687dde946d1039b6fab187a862e85e51aad9
linzj <zuojian.lzj@alibaba-inc.com>
2024-01-05 13:51:52 +0800

    First commit

---
+++ app/src/main/java/com/example/batteryringtone/BatteryService.kt
@@ -0,0 +1,11 @@
+package com.example.batteryringtone
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class BatteryService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
